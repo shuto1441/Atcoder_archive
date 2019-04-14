8904db316534c8b4209818ffa3018eb6c800e931
SHUUTO_187 <shuto1441@gmail.com>
2019-04-14 12:29:48 +0900

    [AC] abc088 abc088_a

---
+++ atcoder.jp/abc088/abc088_a/Main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a,b;
+  cin >> a >> b;
+  if(a % 500 > b)
+    cout << "No" << endl;
+  else
+    cout << "Yes" << endl;
+}
+
+
+
