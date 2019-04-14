d4b85973345fb8a00b095a4ee55a68ed02b3501c
SHUUTO_187 <shuto1441@gmail.com>
2019-04-14 18:40:24 +0900

    [AC] abc050 abc050_a

---
+++ atcoder.jp/abc050/abc050_a/Main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a,c;
+  char b;
+  cin >> a >> b >>c;
+  if(b=='+')
+      cout << a + c <<endl;
+  else
+      cout<<a-c<<endl;
+}
