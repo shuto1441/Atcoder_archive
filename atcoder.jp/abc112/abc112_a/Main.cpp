dac249ae16dcfd0d6ebd4745c57c2a0dca7c99d0
SHUUTO_187 <shuto1441@gmail.com>
2019-04-15 23:08:21 +0900

    [AC] abc112 abc112_a

---
+++ atcoder.jp/abc112/abc112_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int a;
+  int b,c;
+  cin >> a>>b>>c;
+  if(a==1)
+    cout<<"Hello World"<<endl;
+  else
+    cout<<b+c<<endl;
+}
