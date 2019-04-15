4af3ef474411f89a2766cfe6cba0f2c72e53a34c
SHUUTO_187 <shuto1441@gmail.com>
2019-04-15 22:41:09 +0900

    [AC] abc119 abc119_a

---
+++ atcoder.jp/abc119/abc119_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  if((s.at(5)=='0'&&s.at(6)=='4')||(s.at(5)=='0'&&s.at(6)=='3')||(s.at(5)=='0'&&s.at(6)=='2')||(s.at(5)=='0'&&s.at(6)=='1'))
+     cout << "Heisei" << endl;
+  else
+    cout<<"TBD"<<endl;
+}
+
