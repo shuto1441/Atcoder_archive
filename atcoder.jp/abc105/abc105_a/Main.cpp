64d90a98b9bfdc66970c6237a4d690d92f4a357f
SHUUTO_187 <shuto1441@gmail.com>
2019-04-15 23:31:04 +0900

    [AC] abc105 abc105_a

---
+++ atcoder.jp/abc105/abc105_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int a,b;
+  int ans;
+  cin >> a>>b;
+  if(a%b==0)
+    ans = 0;
+  else
+    ans =1;
+
+
+  cout<<ans<<endl;
+}
+
