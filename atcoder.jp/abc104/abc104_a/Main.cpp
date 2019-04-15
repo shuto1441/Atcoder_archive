66d1d34b3e56fb62297a73aa2f201f9b649471da
SHUUTO_187 <shuto1441@gmail.com>
2019-04-15 23:39:24 +0900

    [AC] abc104 abc104_a

---
+++ atcoder.jp/abc104/abc104_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int a;
+  cin >> a;
+
+  if(a<1200)
+    cout<<"ABC"<<endl;
+  else if(a>=2800)
+    cout<<"AGC"<<endl;
+  else
+    cout<<"ARC"<<endl;
+
+}
+
