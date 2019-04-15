665045c48fc880b6e52b920fb0f1de847e71be6b
SHUUTO_187 <shuto1441@gmail.com>
2019-04-15 23:15:05 +0900

    [AC] abc111 abc111_a

---
+++ atcoder.jp/abc111/abc111_a/Main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  string a;
+  cin >> a;
+  for(int i=0;i<3;i++)
+    if(a.at(i)=='1')
+      a.at(i)='9';
+    else
+      a.at(i)='1';
+  cout<<a<<endl;
+}
+
