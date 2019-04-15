65c0cf700ce2d836ac75efe966a5b89b9991de8f
SHUUTO_187 <shuto1441@gmail.com>
2019-04-15 22:58:14 +0900

    [AC] abc115 abc115_a

---
+++ atcoder.jp/abc115/abc115_a/Main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a;
+  cin >> a;
+  if(a==22)
+    cout << "Christmas Eve Eve Eve"<<endl;
+  else if(a==23)
+     cout << "Christmas Eve Eve"<<endl;
+  else if(a==24)
+    cout << "Christmas Eve"<<endl;
+  else
+     cout << "Christmas"<<endl;
+    
+ 
+}
+
+
