de230652ed71bcef359fab564ff4c49140ab71be
SHUUTO_187 <shuto1441@gmail.com>
2019-04-17 00:29:34 +0900

    [AC] abc077 abc077_a

---
+++ atcoder.jp/abc077/abc077_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string a,b;
+  cin >> a>>b;
+  if(a.at(0)==b.at(2)&&a.at(1)==b.at(1)&&a.at(2)==b.at(0))
+    cout<<"YES"<<endl;
+  else
+    cout<<"NO"<<endl;
+  
+  
+
+}
+
