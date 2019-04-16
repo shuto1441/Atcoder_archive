5513d6a45742b6d67f9e4539a9aaf975cf1bd317
SHUUTO_187 <shuto1441@gmail.com>
2019-04-17 00:13:06 +0900

    [AC] abc081 abc081_a

---
+++ atcoder.jp/abc081/abc081_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  string s;
+  cin >> s;
+  int cnt=0;
+  for(int i=0;i<3;i++){
+    if(s.at(i)=='1')
+       cnt++;
+  }
+  cout<<cnt<<endl;
+  
+  
+
+}
+
