41b87d5c2087dae29db6b39a54cfc169b03bb3a2
SHUUTO_187 <shuto1441@gmail.com>
2019-04-21 10:39:01 +0900

    [AC] abc042 abc042_a

---
+++ atcoder.jp/abc042/abc042_a/Main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a,b,c;
+  cin>>a>>b>>c;
+  if(a==5&&b==5&&c==7||a==7&&b==5&&c==5||a==5&&b==7&&c==5)
+    cout<<"YES"<<endl;
+  else
+    cout<<"NO"<<endl;
+
+}
