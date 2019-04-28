aea62a94c8acf04aeed6c8fd51d0ec9a57a81776
SHUUTO_187 <shuto1441@gmail.com>
2019-04-28 19:06:59 +0900

    [AC] abc028 abc028_a

---
+++ atcoder.jp/abc028/abc028_a/Main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+  
+  if(n<60)
+    cout<<"Bad"<<endl;
+  else if(n<90)
+    cout<<"Good"<<endl;
+  else if(n<100)
+    cout<<"Great"<<endl;
+  else
+    cout<<"Perfect"<<endl;
+
+}
+
