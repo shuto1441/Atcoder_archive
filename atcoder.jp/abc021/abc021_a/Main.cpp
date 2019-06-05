48d362ee1dbb4ddd4b88da573ab96b6d9e55c232
SHUUTO_187 <shuto1441@gmail.com>
2019-06-05 13:13:44 +0900

    [AC] abc021 abc021_a

---
+++ atcoder.jp/abc021/abc021_a/Main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin>>N;
+  cout<<(N+1)/2<<endl;
+  if(N%2==1)
+    cout<<1<<endl;
+  for(int i=0;i<N/2;i++)
+    cout<<2<<endl;
+  
+}
+
+      
+
+
+
