d2072fa546fb78de90edcca66af805b144a8f672
SHUUTO_187 <shuto1441@gmail.com>
2019-06-05 13:30:22 +0900

    [AC] abc016 abc016_1

---
+++ atcoder.jp/abc016/abc016_1/Main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N,M;
+  cin>>N>>M;
+  if(N%M==0)
+    cout<<"YES"<<endl;
+  else
+    cout<<"NO"<<endl;
+  
+  
+}
+
+      
+
+
+
