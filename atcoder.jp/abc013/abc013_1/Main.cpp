6121796f3f2b437ab6ea934adb66c0f47a3aa9b1
SHUUTO_187 <shuto1441@gmail.com>
2019-06-05 13:39:11 +0900

    [AC] abc013 abc013_1

---
+++ atcoder.jp/abc013/abc013_1/Main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  char a;
+  cin>>a;
+  if(a=='A')
+    cout<<1<<endl;
+  if(a=='B')
+    cout<<2<<endl;
+  if(a=='C')
+    cout<<3<<endl;
+  if(a=='D')
+    cout<<4<<endl;
+  if(a=='E')
+    cout<<5<<endl;
+}
+
+      
+
+
+
+
