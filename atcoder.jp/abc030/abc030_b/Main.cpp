95a811f1a6c68f69aa3ebc66b76f8d27a4d32323
SHUUTO_187 <shuto1441@gmail.com>
2019-06-13 11:32:58 +0900

    [AC] abc030 abc030_b

---
+++ atcoder.jp/abc030/abc030_b/Main.cpp
@@ -0,0 +1,36 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define rep(i, s) for (int i = 0; i < s; ++i)
+#define repr(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define all(v) (v.begin(), v.end())
+#define EACH(i, s) for (__typeof__((s).begin()) i = (s).begin(); i != (s).end(); ++i)
+#define VEC(a, n) vector<int>a(n)
+#define PQ(a) priority_queue<int>a
+#define PQmin(a) priority_queue< int, :vector<int>, greater<int> >a
+#define PAIR pair<int, int>
+typedef long long ll;
+typedef pair<ll, ll> l_l;
+typedef pair<int, int> i_i;
+typedef pair<string, int>s_i;
+typedef pair<int, string>i_s;
+#define EPS (1e-7)
+#define INF (1e10)
+#define PI (acos(-1))
+const ll mod = 1000000007;
+ 
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n,m;
+  cin>>n>>m;
+  double a,b;
+  b=6*m;
+  if(n<12)
+    a=30*n+0.5*m;
+  else
+    a=30*(n-12)+0.5*m;
+  double ans=min(abs(a-b),360-abs(a-b));
+  cout<<ans<<endl;
+}
+
