0ddf53e9ed4c331c38abb79c9c569aa8772341ba
SHUUTO_187 <shuto1441@gmail.com>
2019-06-22 21:19:39 +0900

    [AC] abc131 abc131_b

---
+++ atcoder.jp/abc131/abc131_b/Main.cpp
@@ -0,0 +1,54 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define rep(i, s) for (int i = 0; i < s; ++i)
+#define repr(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define all(v) (v.begin(), v.end())
+#define EACH(i, s) for (__typeof__((s).begin()) i = (s).begin(); i != (s).end(); ++i)
+#define VEC(a, n) vector<int>a(n)
+#define PQ(a) priority_queue<int>a
+#define PQmin(a) priority_queue< int, :vector<int>, greater<int> >a
+#define PAIR pair<int, int>
+typedef long long ll;
+typedef pair<ll, ll> l_l;
+typedef pair<int, int> i_i;
+#define EPS (1e-7)
+#define INF (1e10)
+#define PI (acos(-1))
+const ll mod = 1000000007;
+bool f(string s){
+  bool ans=true;
+  rep(i,s.size()/2){
+    if(s[i]!=s[s.size()/2+i]){
+      ans=false;
+      break;
+    }
+  }
+  return ans;
+}
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int N,L;
+  cin>>N>>L;
+  ll sum=(1+N)*N/2-(1-L)*N;
+
+  ll ans;
+  ll min=INF;
+  for(int i=1;i<N+1;i++){
+    if(min>abs(L+i-1)){
+      min=abs(L+i-1);
+      ans=sum-(L+i-1);
+    }
+  }
+  cout<<ans<<endl;
+    
+
+}
+
+
+
+
+
+
