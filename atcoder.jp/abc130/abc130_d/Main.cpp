e5d4536a7e202d73d7a1ad748530ceafd6ca0d42
SHUUTO_187 <shuto1441@gmail.com>
2019-06-23 21:41:14 +0900

    [AC] abc130 abc130_d

---
+++ atcoder.jp/abc130/abc130_d/Main.cpp
@@ -0,0 +1,63 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define rep(i, s) for (int i = 0; i < s; ++i)
+#define repr(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define all(v) (v.begin(), v.end())
+#define EACH(i, s) for (__typeof__((s).begin()) i = (s).begin(); i != (s).end(); ++i)
+#define VEC(a, n) vector<int>a(n)
+#define PQ(a) priority_queue<int>a
+#define PQmin(a) priority_queue< int, :vector<int>, greater<int> >a
+#define PAIR pair<int, int>
+typedef long long ll;
+typedef pair<ll, ll> l_l;
+typedef pair<int, int> i_i;
+#define EPS (1e-7)
+#define INF (1e10)
+#define PI (acos(-1))
+const ll mod = 1000000007;
+bool f(string s){
+  bool ans=true;
+  rep(i,s.size()/2){
+    if(s[i]!=s[s.size()/2+i]){
+      ans=false;
+      break;
+    }
+  }
+  return ans;
+}
+
+
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int N;
+  ll K;
+  cin>>N>>K;
+  vector<ll>A(N);
+  rep(i,N){
+    cin>>A[i];
+  }
+  ll ans=0,sum=0;
+  int r=0;
+  rep(i,N){
+    while(sum<K){
+      if(r==N)
+       break;
+      else{
+        sum+=A[r];
+        r++;
+      }
+    }
+    if(sum<K)
+       break;
+    ans+=N-r+1;
+    sum-=A[i];
+  }
+  cout<<ans<<endl;
+
+}
+    
+  
+  
