c242e0a43d045a2f8d9233b007655cbb47b0c09c
SHUUTO_187 <shuto1441@gmail.com>
2020-04-08 18:07:42 +0900

    [AC] abc155 abc155_a

---
+++ atcoder.jp/abc155/abc155_a/Main.cpp
@@ -0,0 +1,40 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define rep(i, s) for (int i = 0; i < s; ++i)
+#define repr(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define all(v) (v.begin(), v.end())
+#define EACH(i, s) for (__typeof__((s).begin()) i = (s).begin(); i != (s).end(); ++i)
+#define VEC(a, n) vector<int>a(n)
+#define PQ(a) priority_queue<int>a
+#define PQmin(a) priority_queue< int, :vector<int>, greater<int> >a
+#define PAIR pair<int, int>
+typedef long long ll;
+typedef pair<ll, ll> l_l;
+typedef pair<int, int> i_i;
+#define EPS (1e-7)
+#define INF (1e10)
+#define PI (acos(-1))
+const ll mod = 1000000007;
+bool f(string s){
+  bool ans=true;
+  rep(i,s.size()/2){
+    if(s[i]!=s[s.size()/2+i]){
+      ans=false;
+      break;
+    }
+  }
+  return ans;
+}
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int N,R,M;
+  cin>>N>>R>>M;
+  if((N==R&&N!=M)||(N==M&&N!=R)||(M==R&&N!=M))
+    cout<<"Yes"<<endl;
+  else
+    cout<<"No"<<endl;
+
+}
