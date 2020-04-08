3519fcf6baa090d6a814c344bd06ecf67757c45c
SHUUTO_187 <shuto1441@gmail.com>
2020-04-08 18:14:06 +0900

    [AC] abc154 abc154_a

---
+++ atcoder.jp/abc154/abc154_a/Main.cpp
@@ -0,0 +1,48 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define rep(i, s) for (int i = 0; i < s; ++i)
+#define repr(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define all(v) (v.begin(), v.end())
+#define EACH(i, s) for (__typeof__((s).begin()) i = (s).begin(); i != (s).end(); ++i)
+#define VEC(a, n) vector<int>a(n)
+#define PQ(a) priority_queue<int>a
+#define PQmin(a) priority_queue< int, :vector<int>, greater<int> >a
+#define PAIR pair<int, int>
+typedef long long ll;
+typedef pair<ll, ll> l_l;
+typedef pair<int, int> i_i;
+#define EPS (1e-7)
+#define INF (1e10)
+#define PI (acos(-1))
+const ll mod = 1000000007;
+bool f(string s){
+  bool ans=true;
+  rep(i,s.size()/2){
+    if(s[i]!=s[s.size()/2+i]){
+      ans=false;
+      break;
+    }
+  }
+  return ans;
+}
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int A,B;
+  string N,M,P;
+  cin>>N>>M>>A>>B>>P;
+  if(N==P){
+    cout<<A-1<<endl;
+    cout<<B<<endl;
+  }
+  else if(M==P){
+    cout<<A<<endl;
+    cout<<B-1<<endl;
+  }
+  else{
+    cout<<A<<endl;
+    cout<<B<<endl;
+  }
+}
