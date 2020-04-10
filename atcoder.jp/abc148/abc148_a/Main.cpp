6e5227b85c4453e71610914113a84c9affaa898f
SHUUTO_187 <shuto1441@gmail.com>
2020-04-10 13:01:03 +0900

    [AC] abc148 abc148_a

---
+++ atcoder.jp/abc148/abc148_a/Main.cpp
@@ -0,0 +1,44 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define rep(i, s) for (int i = 0; i < s; ++i)
+#define repr(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define all(v) (v.begin(), v.end())
+#define EACH(i, s) for (__typeof__((s).begin()) i = (s).begin(); i != (s).end(); ++i)
+#define VEC(a, n) vector<int>a(n)
+#define PQ(a) priority_queue<int>a
+#define PQmin(a) priority_queue< int, :vector<int>, greater<int> >a
+#define PAIR pair<int, int>
+typedef long long ll;
+typedef pair<ll, ll> l_l;
+typedef pair<int, int> i_i;
+#define EPS (1e-7)
+#define INF (1e10)
+#define PI (acos(-1))
+const ll mod = 1000000007;
+bool f(string s){
+  bool ans=true;
+  rep(i,s.size()/2){
+    if(s[i]!=s[s.size()/2+i]){
+      ans=false;
+      break;
+    }
+  }
+  return ans;
+}
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int K,X;
+  cin>>K>>X;
+  if(K!=1&&X!=1)
+    cout<<1<<endl;
+  else if(K!=2&&X!=2)
+    cout<<2<<endl;
+  else
+    cout<<3<<endl;
+
+
+
+}
