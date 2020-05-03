8dc0ec06fe1dca4c74a4c348cdbbddb3c988e6c0
SHUUTO_187 <shuto1441@gmail.com>
2020-05-03 21:12:46 +0900

    [AC] abc166 abc166_b

---
+++ atcoder.jp/abc166/abc166_b/Main.cpp
@@ -0,0 +1,54 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define rep(i, s) for (int i = 0; i < s; ++i)
+#define repr(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define all(v) (v.begin(), v.end())
+#define EACH(i, s) for (__typeof__((s).begin()) i = (s).begin(); i != (s).end(); ++i)
+#define VEC(a, n) vector<int>a(n)
+#define VECV(a,n,m) vector<vector<int>> a(n, vector<int>(m));
+#define PQ(a) priority_queue<int>a
+#define PQmin(a) priority_queue< int, :vector<int>, greater<int> >a
+#define PAIR pair<int, int>
+typedef long long ll;
+typedef pair<ll, ll> l_l;
+typedef pair<int, int> i_i;
+#define EPS (1e-7)
+#define INF (1e10)
+#define PI (acos(-1))
+const ll mod = 1000000007;
+bool f(string s){
+  bool ans=true;
+  rep(i,s.size()/2){
+    if(s[i]!=s[s.size()/2+i]){
+      ans=false;
+      break;
+    }
+  }
+  return ans;
+}
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int N,K;
+  cin>>N>>K;
+  int h[N];
+  rep(i,N) h[i]=0;
+  rep(i,K){
+    int d;
+    cin>>d;
+    rep(j,d){
+      int a;
+      cin>>a;
+      h[a-1]=1;
+    }
+  }
+  int ans=0;
+  rep(i,N){
+    if(h[i]==0)
+      ans+=1;
+  }
+  cout<<ans<<endl;
+      
+
+}
