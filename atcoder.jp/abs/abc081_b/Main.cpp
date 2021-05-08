445e5b0934df454859e691b5a8d87099a4b2b2d0
SHUUTO_187 <shuto1441@gmail.com>
2021-05-08 23:39:36 +0900

    [AC] abs abc081_b

---
+++ atcoder.jp/abs/abc081_b/Main.cpp
@@ -0,0 +1,41 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define rep(i, s) for (int i = 0; i < s; ++i)
+#define repr(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define all(v) (v.begin(), v.end())
+#define EACH(i, s) for (__typeof__((s).begin()) i = (s).begin(); i != (s).end(); ++i)
+#define VEC(a, n) vector<int>a(n)
+#define VECV(a,n,m) vector<vector<int>> a(n, vector<int>(m));
+#define PQ(a) priority_queue<int>a
+#define PQmin(a) priority_queue< int, :vector<int>, greater<int> >a
+#define PAIR pair<int, int>
+typedef long long ll;
+typedef pair<ll, ll> l_l;
+typedef pair<int, int> i_i;
+#define EPS (1e-7)
+#define INF (1e10)
+#define PI (acos(-1))
+const ll mod = 1000000007;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int N;
+  ll A[N];
+  cin>>N;
+  rep(i,N) cin>>A[i];
+  int num[N];
+  int cnt;
+  rep(i,N){
+    cnt=0;
+    while(A[i]%2==0){
+      A[i]/=2;
+      cnt+=1;
+    }
+    num[i]=cnt;
+  }
+  cout<<*min_element(num,num+N)<<endl;
+  
+  return 0;
+}
