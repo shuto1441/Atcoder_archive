7c9916dd1b25de3234e61b143799b981c7e0fb1d
SHUUTO_187 <shuto1441@gmail.com>
2021-05-22 22:16:42 +0900

    [AC] abc202 abc202_c

---
+++ atcoder.jp/abc202/abc202_c/Main.cpp
@@ -0,0 +1,54 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define rep(i, s) for (int i = 0; i < s; ++i)
+#define repr(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define all(v) (v.begin(), v.end())
+#define EACH(i, s) for (__typeof__((s).begin()) i = (s).begin(); i != (s).end(); ++i)
+#define VEC(a, n) vector<int>a(n)
+#define VECV(a,n,m) vector<vector<int>> a(n, vector<int>(m));
+#define PQ(a) priority_queue<int>a
+#define PQmin(a) priority_queue< int, :vector<int>, greater<int> >a
+#define PAIR pair<int, int>
+#define SET(value) set<int>value
+#define SORT_G(a,N) sort(a,a+N,greater<int>())
+typedef long long ll;
+typedef pair<ll, ll> l_l;
+typedef pair<int, int> i_i;
+#define EPS (1e-7)
+#define INF (1e10)
+#define PI (acos(-1))
+const ll mod = 1000000007;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  ll N;
+  cin>>N;
+  ll A[N+1],B[N+1],C[N+1];
+  FOR(i,1,N+1) cin>>A[i]; 
+  FOR(i,1,N+1) cin>>B[i];
+  FOR(i,1,N+1) cin>>C[i];
+  ll cnt=0;
+  ll D[N+1],E[N+1];
+  rep(i,N+1){
+    D[i]=0;
+    E[i]=0;
+  }
+  FOR(i,1,N+1){
+    D[B[C[i]]]+=1;
+  }
+  FOR(i,1,N+1){
+    E[A[i]]+=1;
+  }
+
+  FOR(i,1,N+1){
+    cnt+=D[i]*E[i];
+  }
+  
+
+  cout<<cnt<<endl;
+  
+  
+  return 0;
+}
